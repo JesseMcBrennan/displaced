f13526bb5ae187cd5b0d9c4e0ea8fa56159a079e
Jesse McFadden <jessemcbrennan@gmail.com>
2018-09-11 08:39:31 -0600

    Add NavBar component

---
+++ src/containers/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, NavLink } from 'react-router-dom';
+import './NavBar.css'
+
+export const NavBar = () => {
+  return(
+    <ul className='navBar'>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  )
+}
