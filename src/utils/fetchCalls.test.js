65e6c01978ce0f09c1b1658b029f2abd4915c949
Jesse McFadden <jessemcbrennan@gmail.com>
2018-09-12 08:50:26 -0600

    Add basic testing to fetchcalls

---
+++ src/utils/fetchCalls.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import { membersFetch, membersMoreInfo } from './fetchCalls'
+
+describe('membersFetch', () => {
+  it('should render without crashing', () => {
+    
+  })
+})
