b3c9c7febe9c0631a97ec7c08872dd95745d8978
Jesse McFadden <jessemcbrennan@gmail.com>
2018-09-12 08:48:39 -0600

    Add template for app.test.js tests

---
+++ src/containers/App/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import { App } from './App'
+
+describe('App container', () => {
+  it('should render without crashing', () => {
+    
+  })
+})
