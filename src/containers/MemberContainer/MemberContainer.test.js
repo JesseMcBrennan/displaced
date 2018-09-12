cb995104b26eda005f863acfa14d18facf263ecf
Jesse McFadden <jessemcbrennan@gmail.com>
2018-09-12 08:46:58 -0600

    Add MemberContainer tests

---
+++ src/containers/MemberContainer/MemberContainer.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import { MemberContainer } from './MemberContainer'
+
+describe('MemberContainer container', () => {
+  it('should render without crashing', () => {
+    
+  })
+})
