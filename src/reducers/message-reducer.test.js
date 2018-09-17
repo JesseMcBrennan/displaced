68d5b9132efe231f8b3da5bb8049a149b0e7cb2f
Jesse McFadden <jessemcbrennan@gmail.com>
2018-09-17 17:48:04 -0600

    Add initial messageReducer tests

--- src/reducers/message-reducer.test.js
+++ src/reducers/message-reducer.test.js
@@ -1,10 +1,16 @@
 import { messageReducer } from './message-reducer.js';
-import * actions from '../actions';
+import * as actions from '../actions';
 
 describe('messageReducer', () => {
   it('should return initial state by default', () => {
     const expected = []
     const result = messageReducer(undefined, {});
     expect(result).toEqual(expected)
+  })
+  it('should return an object with memberdat', () => {
+    const mockResult = [{}]
+    const expected = mockResult
+    const result = messageReducer([], actions.setMessage(mockResult))
+    expect(result).toEqual(expect)
   })
 })
--- src/utils/fetchCalls.js
+++ src/utils/fetchCalls.js
@@ -10,6 +10,7 @@ export const membersFetch = async (chamber, state, district) => {
     }
     });
     const result = await response.json()
+    debugger;
     const moreInfo =  await membersMoreInfo(result.results[0].api_uri)
     const fetchedMembersData = moreInfo
     return fetchedMembersData
