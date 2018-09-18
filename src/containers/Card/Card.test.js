e9460d0f8725758a745e07d2daaf2c5767913136
Jesse McFadden <jessemcbrennan@gmail.com>
2018-09-18 15:01:58 -0600

    Add tests for card.js

--- src/containers/Card/Card.test.js
+++ src/containers/Card/Card.test.js
@@ -7,6 +7,7 @@ import { mockMemberData } from '../../utils/mockData/mockMemberData'
 describe('Card', () => {
   let wrapper;
   let mockTitle = {title: 'Senator, 2nd Class'}
+  let mockHistory = { push: jest.fn ()}
 
   beforeEach(() => {
     wrapper = shallow(
